cc4eaffb9b6b1e7815ce3764a7acf5f130541ed6
Nicolasalan <nicolasalangrotti03@gmail.com>
2024-02-10 03:04:04 -0300

    update action

--- microvault/config_robot.py
+++ microvault/config_robot.py
@@ -2,16 +2,6 @@ from typing import Optional
 
 import sentry_sdk
 
-sentry_sdk.init(
-    dsn="https://a543aaf28e1fde91541d9ee60cb16951@o4506720636502016.ingest.sentry.io/4506720639057920",
-    # Set traces_sample_rate to 1.0 to capture 100%
-    # of transactions for performance monitoring.
-    traces_sample_rate=1.0,
-    # Set profiles_sample_rate to 1.0 to profile 100%
-    # of sampled transactions.
-    # We recommend adjusting this value in production.
-    profiles_sample_rate=1.0,
-)
 
 class ConfigRobot:
     def __init__(
