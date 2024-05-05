adaa6f84f57fcfb282ab5cd44138f12134658c9d
Nicolasalan <nicolasalangrotti03@gmail.com>
2024-05-05 12:04:27 -0300

    update docs

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,7 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(".."))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
@@ -10,6 +10,16 @@ project = "Microvault"
 copyright = "2024, Nicolas Alan"
 author = "Nicolas Alan"
 release = "0.2.3"
+
+extensions = [
+    "sphinx.ext.duration",
+    "sphinx.ext.doctest",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.intersphinx",
+    "sphinx_toolbox.collapse",
+    "notfound.extension",
+]
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -23,5 +33,5 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "alabaster"
+html_theme = "furo"
 html_static_path = ["_static"]
--- docs/index.rst
+++ docs/index.rst
@@ -10,7 +10,7 @@ Welcome to Microvault's documentation!
    :maxdepth: 2
    :caption: Contents:
 
-
+    modules
 
 Indices and tables
 ==================
