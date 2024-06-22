5ba0ea539e0755987d698a79bbd72a30346cfd8d
nicolasalan <nicolasalan@gmail.com>
2024-06-22 03:02:19 +0000

    rename engine for engines

---
+++ microvault/engines/sanity.py
@@ -0,0 +1,26 @@
+
+import torch
+
+import numpy as np
+import random
+
+class EngineOps():
+
+    def __init__(self, seed: int = 42, device: str = "cuda"):
+        self.seed = seed
+        self.device = device
+
+    def seed_everything(self):
+        torch.manual_seed(self.seed)
+        np.random.seed(self.seed)
+        random.seed(self.seed)
+
+        torch.cuda.manual_seed_all(self.seed)
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
+
+    def set_device(self):
+        # clear the cache
+        torch.cuda.empty_cache()
+        # Set the device globally
+        torch.set_default_device(self.device)
