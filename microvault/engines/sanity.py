8383c9a76e8d92927f4dc0ed1248933faca2415d
nicolasalan <nicolasalan@gmail.com>
2024-06-30 18:37:01 +0000

    test(engine): check if cuda is available

--- microvault/engines/sanity.py
+++ microvault/engines/sanity.py
@@ -15,12 +15,14 @@ class EngineOps():
         np.random.seed(self.seed)
         random.seed(self.seed)
 
-        torch.cuda.manual_seed_all(self.seed)
-        torch.backends.cudnn.deterministic = True
-        torch.backends.cudnn.benchmark = False
+        if torch.cuda.is_available():
+            torch.cuda.manual_seed_all(self.seed)
+            torch.backends.cudnn.deterministic = True
+            torch.backends.cudnn.benchmark = False
 
     def set_device(self):
-        # clear the cache
-        torch.cuda.empty_cache()
+        if torch.cuda.is_available():
+          # clear the cache
+          torch.cuda.empty_cache()
         # Set the device globally
         torch.set_default_device(self.device)
