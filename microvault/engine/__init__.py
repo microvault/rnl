1ed99ac3c149a4460fb7ba9264e8ea484daa9f0c
Nicolasalan <nicolasalangrotti03@gmail.com>
2024-07-23 05:26:35 -0300

    refactor(collision): refactoring modules into smaller modules

---
+++ microvault/engine/__init__.py
@@ -0,0 +1,9 @@
+from engine.collision import Collision
+from engine.map2d import Map2D
+from engine.world_generate import GenerateWorld
+
+__all__ = [
+    "Map2D",
+    "Collision",
+    "GenerateWorld",
+]
