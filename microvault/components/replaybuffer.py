bd6add4096a2aa2bd3876071ffd58d9b91dacd58
Nicolasalan <nicolasalangrotti03@gmail.com>
2024-07-28 12:14:20 -0300

    refactor(replaybuffer): remove param and PER

--- microvault/components/replaybuffer.py
+++ microvault/components/replaybuffer.py
@@ -1,9 +1,9 @@
-from collections import deque
+import random
+from collections import deque, namedtuple
+from typing import Tuple
 
 import numpy as np
 import torch
-
-from microvault.components.sumtree import SumTree
 
 
 class ReplayBuffer:
@@ -15,32 +15,15 @@ class ReplayBuffer:
         buffer_size: int = 1048576,
         batch_size: int = 32,
         gamma: float = 0.99,
-        nstep: float = 10,
+        nstep: int = 10,
         state_dim: int = 13,
         action_dim: int = 2,
-        epsilon: float = 0.01,
-        alpha: float = 0.6,
-        beta: float = 0.4,
-        beta_increment_per_sampling: float = 1e-4,
-        absolute_error_upper: float = 1.0,
         device: str = "cpu",
     ):
         """
         A árvore é composta por uma árvore de soma que contém as pontuações de prioridade em sua folha e também uma matriz de dados.
         """
 
-        self.tree = SumTree(buffer_size)
-        self.epsilon = epsilon  # small amount to avoid zero priority
-        self.alpha = (
-            alpha  # [0..1] convert the importance of TD error to priority, often 0.6
-        )
-        self.beta = (
-            beta  # importance-sampling, from initial value increasing to 1, often 0.4
-        )
-        self.beta_increment_per_sampling = (
-            beta_increment_per_sampling  # annealing the bias, often 1e-3
-        )
-        self.absolute_error_upper = absolute_error_upper  # clipped abs error
         self.batch_size = batch_size
         self.gamma = gamma
         self.n_step = nstep
@@ -48,12 +31,11 @@ class ReplayBuffer:
         self.state_dim = state_dim
         self.action_dim = action_dim
         self.device = device
-
-    def __len__(self):
-        return len(self.tree)
-
-    def is_full(self):
-        return len(self.tree) >= self.tree.capacity
+        self.memory = deque(maxlen=buffer_size)
+        self.experience = namedtuple(
+            "Experience",
+            field_names=["state", "action", "reward", "next_state", "done"],
+        )
 
     def multistep(self):
         # Retorno do buffer de n-step
@@ -75,7 +57,7 @@ class ReplayBuffer:
         action: np.ndarray = None,
         reward: np.float64 = None,
         next_state: np.float64 = None,
-        done: bool = False,
+        done: np.bool_ = np.bool_(False),
     ):
         assert isinstance(
             state[0], np.float32
@@ -132,148 +114,76 @@ class ReplayBuffer:
         if len(self.n_step_buffer) == self.n_step:
             (state, action, reward, next_state, done) = self.multistep()
 
-        sample = (state, action, reward, next_state, done)
-        if reward is None:
-            priority = np.amax(self.tree.tree[-self.tree.capacity :])
-            if priority == 0:
-                priority = self.absolute_error_upper
-        else:
-            priority = min(
-                (abs(reward) + self.epsilon) ** self.alpha, self.absolute_error_upper
-            )
-        self.tree.add(sample, priority)
+        e = self.experience(state, action, reward, next_state, done)
+        self.memory.append(e)
 
-    def sample(self):
+    def sample(
+        self,
+    ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
         """
         - First, to sample a minibatch of size k the range [0, priority_total] is divided into k ranges.
         - Then a value is uniformly sampled from each range.
         - We search in the sumtree, the experience where priority score correspond to sample values are retrieved from.
         - Then, we calculate IS weights for each minibatch element.
         """
-
-        minibatch = []
-
-        idxs = np.empty((self.batch_size,), dtype=np.int32)
-        is_weights = np.empty((self.batch_size,), dtype=np.float32)
-
-        # Calculate the priority segment
-        # Divide the Range[0, ptotal] into n ranges
-        priority_segment = (
-            self.tree.total_priority / self.batch_size
-        )  # priority segment
-
-        # Increase the beta each time we sample a new minibatch
-        self.beta = np.amin(
-            [1.0, self.beta + self.beta_increment_per_sampling]
-        )  # max = 1
-
-        # Calculate the max_weight
-        p_min = (
-            np.amin(self.tree.tree[-self.tree.capacity :]) / self.tree.total_priority
-        )
-        max_weight = (p_min * self.batch_size) ** (-self.beta)
-        max_weight = max(max_weight, 1e-5)  # Evita divisão por zero
-
-        priority_segment = self.tree.total_priority / self.batch_size
-
-        for i in range(self.batch_size):
-            a, b = priority_segment * i, priority_segment * (i + 1)
-            value = np.random.uniform(a, b)
-
-            index, priority, data = self.tree.get_leaf(value)
-
-            priority = max(priority, 1e-5)  # Evita zero
-            sampling_probabilities = priority / self.tree.total_priority
-            sampling_probabilities = max(sampling_probabilities, 1e-5)  # Evita zero
-
-            is_weights[i] = (
-                np.power(self.batch_size * sampling_probabilities, -self.beta)
-                / max_weight
-            )
-            is_weights[i] = max(is_weights[i], 1e-5)  # Evita valores inválidos
-
-            idxs[i] = index
-            minibatch.append(data)
+        experiences = random.sample(self.memory, k=self.batch_size)
 
         states = (
-            torch.from_numpy(np.vstack([e[0] for e in minibatch if e is not None]))
+            torch.from_numpy(np.vstack([e[0] for e in experiences if e is not None]))
             .float()
             .to(self.device)
         )
         actions = (
-            torch.from_numpy(np.vstack([e[1] for e in minibatch if e is not None]))
+            torch.from_numpy(np.vstack([e[1] for e in experiences if e is not None]))
             .float()
             .to(self.device)
         )
         rewards = (
-            torch.from_numpy(np.vstack([e[2] for e in minibatch if e is not None]))
+            torch.from_numpy(np.vstack([e[2] for e in experiences if e is not None]))
             .float()
             .to(self.device)
         )
         next_states = (
-            torch.from_numpy(np.vstack([e[3] for e in minibatch if e is not None]))
+            torch.from_numpy(np.vstack([e[3] for e in experiences if e is not None]))
             .float()
             .to(self.device)
         )
         dones = (
             torch.from_numpy(
-                np.vstack([e[4] for e in minibatch if e is not None]).astype(np.uint8)
+                np.vstack([e[4] for e in experiences if e is not None]).astype(np.uint8)
             )
-            .float()
+            .int()
             .to(self.device)
         )
 
-        # assert isinstance(
-        #     states, torch.Tensor
-        # ), "State is not of type torch.Tensor in REPLAY BUFFER."
-        # assert isinstance(
-        #     actions, torch.Tensor
-        # ), "Actions is not of type torch.Tensor in REPLAY BUFFER."
-        # assert isinstance(
-        #     rewards, torch.Tensor
-        # ), "Rewards is not of type torch.Tensor in REPLAY BUFFER."
-        # assert isinstance(
-        #     next_states, torch.Tensor
-        # ), "Next states is not of type torch.Tensor in REPLAY BUFFER."
-        # assert isinstance(
-        #     dones, torch.Tensor
-        # ), "Dones is not of type torch.Tensor in REPLAY BUFFER."
+        assert (
+            states.dtype == torch.float32
+        ), "The (state) tensor elements are not of type torch.float32 in the REPLAY BUFFER -> {}.".format(
+            states.dtype
+        )
+        assert (
+            actions.dtype == torch.float32
+        ), "The (actions) tensor elements are not of type torch.float32 in the REPLAY BUFFER -> {}.".format(
+            actions.dtype
+        )
+        assert (
+            rewards.dtype == torch.float32
+        ), "The (rewards) tensor elements are not of type torch.float32 in the REPLAY BUFFER -> {}.".format(
+            rewards.dtype
+        )
+        assert (
+            next_states.dtype == torch.float32
+        ), "The (next_states) tensor elements are not of type torch.float32 in the REPLAY BUFFER -> {}.".format(
+            next_states.dtype
+        )
+        assert (
+            dones.dtype == torch.int
+        ), "The (dones) tensor elements are not of type torch.float32 in the REPLAY BUFFER -> {}.".format(
+            dones.dtype
+        )
 
-        # assert (
-        #     states.dtype == torch.float32
-        # ), "The (state) tensor elements are not of type torch.float32 in the REPLAY BUFFER -> {}.".format(
-        #     states.dtype
-        # )
-        # assert (
-        #     actions.dtype == torch.float32
-        # ), "The (actions) tensor elements are not of type torch.float32 in the REPLAY BUFFER -> {}.".format(
-        #     actions.dtype
-        # )
-        # assert (
-        #     rewards.dtype == torch.float32
-        # ), "The (rewards) tensor elements are not of type torch.float32 in the REPLAY BUFFER -> {}.".format(
-        #     rewards.dtype
-        # )
-        # assert (
-        #     next_states.dtype == torch.float32
-        # ), "The (next_states) tensor elements are not of type torch.float32 in the REPLAY BUFFER -> {}.".format(
-        #     next_states.dtype
-        # )
-        # assert (
-        #     dones.dtype == torch.int
-        # ), "The (dones) tensor elements are not of type torch.float32 in the REPLAY BUFFER -> {}.".format(
-        #     dones.dtype
-        # )
+        return states, actions, rewards, next_states, dones
 
-        return idxs, states, actions, rewards, next_states, dones, is_weights
-
-    def batch_update(self, idxs, errors) -> None:
-        """
-        Update the priorities on the tree
-        """
-        errors = errors + self.epsilon
-        clipped_errors = np.minimum(errors, self.absolute_error_upper)
-        ps = np.power(clipped_errors, self.alpha)
-
-        for idx, p in zip(idxs, ps):
-            self.tree.update(idx, p)
+    def __len__(self) -> int:
+        """Return the current size of internal memory."""
+        return len(self.memory)
