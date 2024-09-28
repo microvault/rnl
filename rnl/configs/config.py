578c88d1358904b8c1847386051c27fded5bafab
Nicolasalan <nicolasalangrotti03@gmail.com>
2024-09-28 12:18:08 -0300

    Update the project parameter

--- rnl/configs/config.py
+++ rnl/configs/config.py
@@ -1,90 +1,107 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from typing import List
 
-# @dataclass
-# class WandbConfig:
-#     project: str
-#     entity: str
-#     mode: str
-#     use_sweep: bool
-
-
-# @dataclass
-# class AgentConfig:
-#     gamma: float
-#     tau: float
-#     update_every_step: int
-#     lr_model: float
-#     weight_decay: float
-
-
-# @dataclass
-# class ReplayBufferConfig:
-#     buffer_size: int
-
-
-# @dataclass
-# class EnvironmentConfig:
-#     timestep: int
-#     size: float
-#     fps: int
-#     random: int
-#     threshold: float
-#     grid_lenght: int
-#     state_size: int
-#     action_size: int
+import numpy as np
 
 
 @dataclass
 class EnvConfig:
-    map: str
-    mode: str
-    timestep: int
-    fps: int
-    threshold: float
-    grid_lenght: int
-    physical: str
-    rgb_array: bool
+    map_file: str = "rnl/data/map/map.pgm"
+    random_mode: str = "normal"
+    timestep: int = 1000
+    grid_dimension: int = 5
+    friction: float = 0.4
+    porcentage_obstacles: float = 0.1
+    max_step: int = 1000
+
+
+@dataclass
+class RenderConfig:
+    fps: int = 100
+    controller: bool = False
+    rgb_array: bool = False
 
 
 @dataclass
 class SensorConfig:
-    fov: float
-    num_rays: int
-    min_range: float
-    max_range: float
+    fov: float = 4 * np.pi
+    num_rays: int = 20
+    min_range: float = 6.0
+    max_range: float = 1.0
 
 
 @dataclass
 class RobotConfig:
-    radius: float
-    vel_linear: dict
-    vel_angular: dict
+    base_radius: float = 0.033
+    vel_linear: List[float] = field(default_factory=lambda: [0.0, 1.0])
+    vel_angular: List[float] = field(default_factory=lambda: [-1.0, 1.0])
+    wheel_distance: float = 0.16
+    weight: float = 1.0
+    threshold: float = 0.01
 
 
-# @dataclass
-# class EngineConfig:
-#     seed: int
-#     device: str
-#     batch_size: int
-#     pretrained: bool
-#     path: str
-#     wandb: bool
-#     save_checkpoint: int
-#     epochs: int
+@dataclass
+class NetworkConfig:
+    hidden_size: List[int] = field(default_factory=lambda: [800, 600])
 
 
-# @dataclass
-# class NetworkConfig:
-#     layers_model_l1: int
-#     layers_model_l2: int
+@dataclass
+class AgentConfig:
+    max_timestep: int = 800000
+    memory_size: int = 1000000
+    gamma: float = 0.99
+    n_step: int = 3
+    alpha: float = 0.6
+    beta: float = 0.4
+    tau: float = 0.001
+    prior_eps: float = 0.000001
+    num_atoms: int = 51
+    v_min: int = -200
+    v_max: int = 200
+    epsilon_start: float = 1.0
+    epsilon_end: float = 0.1
+    epsilon_decay: float = 0.995
 
 
-# @dataclass
-# class TrainerConfig:
-#     wandb: WandbConfig
-#     agent: AgentConfig
-#     replay_buffer: ReplayBufferConfig
-#     environment: EnvironmentConfig
-#     engine: EngineConfig
-#     network: NetworkConfig
-#     robot: RobotConfig
+@dataclass
+class TrainerConfig:
+    batch_size: int = 64
+    lr: float = 0.0001
+    seed: int = 1
+    num_envs: int = 1
+    device: str = "mps"
+    learn_step: int = 10
+    target_score: float = 200.0
+    max_steps: int = 1000000
+    evaluation_steps: int = 10000
+    evaluation_loop: int = 1
+    learning_delay: int = 0
+    n_step_memory: int = 1
+    checkpoint: int = 100
+    checkpoint_path: str = "checkpoints"
+    overwrite_checkpoints: bool = False
+    use_wandb: bool = False
+    wandb_api_key: str = ""
+    accelerator: bool = False
+
+
+@dataclass
+class HPOConfig:
+    use_mutation: bool = True
+    freq_evolution: int = 10000
+    population_size: int = 6
+    no_mutation: float = 0.4
+    arch_mutation: float = 0.2
+    new_layer: float = 0.2
+    param_mutation: float = 0.2
+    active_mutation: float = 0
+    hp_mutation: float = 0.2
+    hp_mutation_selection: List[str] = field(
+        default_factory=lambda: ["lr", "batch_size"]
+    )
+    mutation_strength: float = 0.1
+    evolution_steps: int = 10000
+    save_elite: bool = False
+    elite_path: str = "elite"
+    tourn_size: int = 2
+    elitism: bool = True
