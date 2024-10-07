4746a28fffcc21383a6674535685e39af392e5c3
Nicolasalan <nicolasalangrotti03@gmail.com>
2024-10-07 20:32:16 -0300

    create noise

---
+++ rnl/components/noise.py
@@ -0,0 +1,25 @@
+import copy
+import random
+
+import numpy as np
+
+
+class OUNoise:
+
+    def __init__(self, size, seed, mu=0.0, theta=0.15, sigma=0.2):
+        self.mu = mu * np.ones(size)
+        self.theta = theta
+        self.sigma = sigma
+        self.seed = random.seed(seed)
+        self.reset()
+
+    def reset(self):
+        self.state = copy.copy(self.mu)
+
+    def sample(self):
+        x = self.state
+        dx = self.theta * (self.mu - x) + self.sigma * np.array(
+            [random.random() for i in range(len(x))]
+        )  # atualizar o estado interno
+        self.state = x + dx
+        return self.state
