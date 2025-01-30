99b4e0e42957616dc2a90fb5279eb9bfb9ac22fa
Nicolasalan <nicolasalangrotti03@gmail.com>
2025-01-30 00:39:45 -0300

    update train

--- Makefile
+++ Makefile
@@ -3,6 +3,30 @@ TAG = latest
 IMAGE = $(IMAGE_NAME):$(TAG)
 VERSION = 1.1
 
+MODE ?= learn
+ALGORITHM ?= PPO
+MAX_TIMESTEP_GLOBAL ?= 10000
+SEED ?= 42
+BUFFER_SIZE ?= 1000000
+HIDDEN_SIZE ?= 256 256
+ACTIVATION ?= LeakyReLU
+BATCH_SIZE ?= 1024
+NUM_ENVS ?= 4
+DEVICE ?= cpu
+CHECKPOINT ?= 29_01_2025
+USE_WANDB ?= False
+WANDB_API_KEY ?=
+LR ?= 0.0003
+LEARN_STEP ?= 512
+GAE_LAMBDA ?= 0.95
+ACTION_STD_INIT ?= 0.6
+CLIP_COEF ?= 0.2
+ENT_COEF ?= 0.0
+VF_COEF ?= 0.5
+MAX_GRAD_NORM ?= 0.5
+UPDATE_EPOCHS ?= 10
+NAME ?= rnl
+
 .PHONY: sim
 sim:
 	@poetry run python -m main sim
@@ -49,11 +73,42 @@ install:
 
 .PHONY: build
 build:
-	@sudo docker build -f docker/Dockerfile -t rnl-docker-cuda .
+	@docker build -f docker/Dockerfile -t rnl-docker-cuda .
 
 .PHONY: train
 train:
-	@sudo docker run -d -e WANDB_API_KEY=$(WANDB_API_KEY) --gpus all --network host --privileged --memory=16g -it -v $(PWD):/workdir rnl-docker-cuda
+	@docker run -d \
+		-e WANDB_API_KEY=$(WANDB_API_KEY) \
+		--gpus all \
+		--network host \
+		--privileged \
+		--memory=16g \
+		-it \
+		-v $(PWD):/workdir \
+		$(IMAGE_NAME) \
+		$(MODE) \
+		--algorithm $(ALGORITHM) \
+		--max_timestep_global $(MAX_TIMESTEP_GLOBAL) \
+		--seed $(SEED) \
+		--buffer_size $(BUFFER_SIZE) \
+		--hidden_size $(HIDDEN_SIZE) \
+		--activation $(ACTIVATION) \
+		--batch_size $(BATCH_SIZE) \
+		--num_envs $(NUM_ENVS) \
+		--device $(DEVICE) \
+		--checkpoint $(CHECKPOINT) \
+		--use_wandb $(USE_WANDB) \
+		--wandb_api_key $(WANDB_API_KEY) \
+		--lr $(LR) \
+		--learn_step $(LEARN_STEP) \
+		--gae_lambda $(GAE_LAMBDA) \
+		--action_std_init $(ACTION_STD_INIT) \
+		--clip_coef $(CLIP_COEF) \
+		--ent_coef $(ENT_COEF) \
+		--vf_coef $(VF_COEF) \
+		--max_grad_norm $(MAX_GRAD_NORM) \
+		--update_epochs $(UPDATE_EPOCHS) \
+		--name $(NAME)
 
 .PHONY: clean
 clean:
--- main.py
+++ main.py
@@ -50,28 +50,28 @@ def main(arg):
 
         # 6.step -> train robot
         model.learn(
-            algorithm="PPO",
-            max_timestep_global=10000, # 1000000
-            seed=42,
-            buffer_size=1000000,
-            hidden_size=[256, 256],
-            activation="LeakyReLU",  # LeakyReLU, ReLU
-            batch_size=1024,
-            num_envs=4,
-            device="cpu",
-            checkpoint="29_01_2025",
-            use_wandb=True,
-            wandb_api_key=str(wandb_key),
-            lr=0.0003,
-            learn_step=512,
-            gae_lambda=0.95,
-            action_std_init=0.6,
-            clip_coef=0.2,
-            ent_coef=0.0,
-            vf_coef=0.5,
-            max_grad_norm=0.5,
-            update_epochs=10,
-            name="rnl",
+            algorithm=args.algorithm,
+            max_timestep_global=args.max_timestep_global,
+            seed=args.seed,
+            buffer_size=args.buffer_size,
+            hidden_size=args.hidden_size,
+            activation=args.activation,
+            batch_size=args.batch_size,
+            num_envs=args.num_envs,
+            device=args.device,
+            checkpoint=args.checkpoint,
+            use_wandb=args.use_wandb,
+            wandb_api_key=args.wandb_api_key,
+            lr=args.lr,
+            learn_step=args.learn_step,
+            gae_lambda=args.gae_lambda,
+            action_std_init=args.action_std_init,
+            clip_coef=args.clip_coef,
+            ent_coef=args.ent_coef,
+            vf_coef=args.vf_coef,
+            max_grad_norm=args.max_grad_norm,
+            update_epochs=args.update_epochs,
+            name=args.name,
         )
 
     elif args.mode == "sim":
@@ -98,6 +98,72 @@ if __name__ == "__main__":
     parser.add_argument(
         "mode", choices=["learn", "sim", "run"], help="Mode to run: 'train' or 'run'"
     )
+
+    parser.add_argument('--algorithm', type=str, default="PPO",
+                        help='Algoritmo de aprendizado a ser usado (default: PPO)')
+
+    parser.add_argument('--max_timestep_global', type=int, default=10000,
+                        help='Número máximo de timesteps globais (default: 10000)')
+
+    parser.add_argument('--seed', type=int, default=42,
+                        help='Semente para inicialização aleatória (default: 42)')
+
+    parser.add_argument('--buffer_size', type=int, default=1000000,
+                        help='Tamanho do buffer (default: 1000000)')
+
+    parser.add_argument('--hidden_size', type=int, nargs='+', default=[256, 256],
+                        help='Tamanhos das camadas ocultas (default: [256, 256])')
+
+    parser.add_argument('--activation', type=str, choices=['LeakyReLU', 'ReLU'], default="LeakyReLU",
+                        help='Função de ativação a ser usada (default: LeakyReLU)')
+
+    parser.add_argument('--batch_size', type=int, default=1024,
+                        help='Tamanho do batch (default: 1024)')
+
+    parser.add_argument('--num_envs', type=int, default=4,
+                        help='Número de ambientes paralelos (default: 4)')
+
+    parser.add_argument('--device', type=str, default="cpu",
+                        help='Dispositivo para treinamento (default: cpu)')
+
+    parser.add_argument('--checkpoint', type=str, default="29_01_2025",
+                        help='Nome do checkpoint (default: 29_01_2025)')
+
+    parser.add_argument('--use_wandb', action='store_true',
+                        help='Ativar o uso do Weights & Biases')
+
+    parser.add_argument('--wandb_api_key', type=str, default="",
+                        help='Chave API do Weights & Biases')
+
+    parser.add_argument('--lr', type=float, default=0.0003,
+                        help='Taxa de aprendizado (default: 0.0003)')
+
+    parser.add_argument('--learn_step', type=int, default=512,
+                        help='Número de passos de aprendizado (default: 512)')
+
+    parser.add_argument('--gae_lambda', type=float, default=0.95,
+                        help='Lambda para GAE (default: 0.95)')
+
+    parser.add_argument('--action_std_init', type=float, default=0.6,
+                        help='Desvio padrão inicial para as ações (default: 0.6)')
+
+    parser.add_argument('--clip_coef', type=float, default=0.2,
+                        help='Coeficiente de clipping (default: 0.2)')
+
+    parser.add_argument('--ent_coef', type=float, default=0.0,
+                        help='Coeficiente de entropia (default: 0.0)')
+
+    parser.add_argument('--vf_coef', type=float, default=0.5,
+                        help='Coeficiente de valor de função (default: 0.5)')
+
+    parser.add_argument('--max_grad_norm', type=float, default=0.5,
+                        help='Norma máxima do gradiente (default: 0.5)')
+
+    parser.add_argument('--update_epochs', type=int, default=10,
+                        help='Número de épocas de atualização (default: 10)')
+
+    parser.add_argument('--name', type=str, default="rnl",
+                        help='Nome do experimento/modelo (default: rnl)')
 
     args = parser.parse_args()
     main(args)
