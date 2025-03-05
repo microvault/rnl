22f3d12dc879985a25968c2c96c2a9338e9ab28b
Nicolasalan <nicolasalangrotti03@gmail.com>
2025-03-05 10:38:41 -0300

    Add parameter

--- rnl/engine/vector.py
+++ rnl/engine/vector.py
@@ -1,8 +1,9 @@
 import gymnasium as gym
 
+from rnl.configs.actions import get_actions_class
 from rnl.configs.config import EnvConfig, RenderConfig, RobotConfig, SensorConfig
+from rnl.configs.rewards import RewardConfig
 from rnl.environment.env import NaviEnv
-from rnl.configs.actions import FastActions, SlowActions
 
 
 def make_vect_envs(
@@ -12,6 +13,8 @@ def make_vect_envs(
     env_config: EnvConfig,
     render_config: RenderConfig,
     use_render: bool,
+    actions_type: get_actions_class,
+    reward_type: RewardConfig,
 ):
     """Returns async-vectorized gym environments with custom parameters.
 
@@ -25,14 +28,18 @@ def make_vect_envs(
 
     def make_env(i):
         def _init():
-            slow_actions = SlowActions()
-            fast_actions = FastActions()
             env = NaviEnv(
-                robot_config, sensor_config, env_config, render_config, use_render,
-                actions_cfg=slow_actions
+                robot_config,
+                sensor_config,
+                env_config,
+                render_config,
+                use_render,
+                actions_cfg=actions_type,
+                reward_cfg=reward_type,
             )
             env.reset(seed=13 + i)
             return env
+
         return _init
 
     return gym.vector.AsyncVectorEnv([make_env(i) for i in range(num_envs)])
