6f40df90367afb19486282136b3cb93e4e6c1a43
Nicolasalan <nicolasalangrotti03@gmail.com>
2025-03-12 00:02:01 -0300

    Getting back to Docker

--- ros/run.bash
+++ ros/run.bash
@@ -1,12 +1,20 @@
 #!/bin/bash
+source /opt/ros/humble/setup.bash
+source /usr/share/gazebo/setup.sh
+
+# Primeiro instalar dependências
+rosdep install --from-paths src --ignore-src -r -y
+
+# Depois limpar e buildar
 rm -rf build/ install/ log/
-colcon build --symlink-install
+colcon build
 
 # Configurar ambiente
-source /Users/nicolasalan/microvault/rnl/ros/install/setup.sh
+source install/setup.bash
 
 export TURTLEBOT3_MODEL=burger
 export LD_LIBRARY_PATH=/usr/local/lib:/opt/ros/humble/lib/libgazebo_ros_factory.so:$LD_LIBRARY_PATH
 export GAZEBO_PLUGIN_PATH=/usr/local/lib:$GAZEBO_PLUGIN_PATH
+
 
 ros2 launch turtlebot3_gazebo turtlebot3_house.launch.py
