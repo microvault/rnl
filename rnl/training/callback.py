6e3a687a7b31b3c0deda05f5b82ca07a2ae5c4b1
Nicolasalan <nicolasalangrotti03@gmail.com>
2025-04-26 00:32:22 -0300

    Update callback.py

--- rnl/training/callback.py
+++ rnl/training/callback.py
@@ -44,14 +44,6 @@ class DynamicTrainingCallback(BaseCallback):
         self.episode_rewards = []
         self.episode_lengths = []
 
-    #     self.sample_file = f"sample_metrics_{self.run_id}.jsonl"
-    #     self._reset_sample_file()  # Limpa o arquivo se já existir
-
-    # def _reset_sample_file(self):
-    #     if os.path.exists(self.sample_file):
-    #         with open(self.sample_file, "w") as f:
-    #             f.write("")
-
     def _init_callback(self) -> None:
         self.start_time = time.time()
         if not os.path.exists(self.model_save_path):
@@ -92,7 +84,6 @@ class DynamicTrainingCallback(BaseCallback):
             self.logger.record("rollout/avg_collision_steps", avg_collision_steps)
             self.logger.record("rollout/avg_goal_steps", avg_goal_steps)
 
-            # Pega infos personalizadas
             infos_list = []
             for i in range(self.model.n_envs):
                 env_info = self.training_env.env_method("get_infos", indices=i)[0]
@@ -111,20 +102,16 @@ class DynamicTrainingCallback(BaseCallback):
                     stats[campo + "_mean"] = media
                     stats[campo + "_std"] = desvio
 
-            # Calcula FPS
             elapsed_time = time.time() - self.start_time
             fps = self.model.num_timesteps / elapsed_time if elapsed_time > 0 else 0
 
-            # Cálculo das médias de reward e episódios
             mean_reward = np.mean(self.episode_rewards) if self.episode_rewards else 0.0
             mean_length = np.mean(self.episode_lengths) if self.episode_lengths else 0.0
 
-            # Logs no logger SB3
             self.logger.record("time/fps", fps)
             self.logger.record("rollout/ep_rew_mean", mean_reward)
             self.logger.record("rollout/ep_len_mean", mean_length)
 
-            # Se o wandb_run existe, faz log no WandB
             if self.wandb_run is not None:
                 wandb_log = {
                     "rollout/success_rate": sucess_rate,
@@ -143,36 +130,12 @@ class DynamicTrainingCallback(BaseCallback):
 
                 self.wandb_run.log(wandb_log, step=self.n_calls)
 
-                if self.n_calls % (self.save_checkpoint * 5) == 0:
-                    save_path = f"{self.model_save_path}/model_{self.n_calls}_steps"
-                    self.model.save(save_path)
+
+            if self.n_calls % (self.save_checkpoint * 5) == 0:
+                save_path = f"{self.model_save_path}/model_{self.n_calls}_steps"
+                print(f"Saving model to {save_path}")
+                self.model.save(save_path)
+                if self.wandb_run is not None:
                     self.wandb_run.save(save_path + ".zip")
-
-        # if self.n_calls % self.sample_checkpoint_freq == 0:
-        #     # Faz uma avaliação rápida
-        #     eval_env = make_environemnt()
-        #     evaluation_results = evaluate_agent(self.model, eval_env)
-        #     (
-        #         sucess_rate, total_timesteps, percentage_unsafe,
-        #         percentage_angular, ep_mean_length, avg_collision_steps,
-        #         avg_goal_steps
-        #     ) = evaluation_results
-        #     merged_metrics = {
-        #         "success_percentage": sucess_rate,
-        #         "total_timesteps": total_timesteps,
-        #         "percentage_unsafe": percentage_unsafe,
-        #         "percentage_angular": percentage_angular,
-        #         "ep_mean_length": ep_mean_length,
-        #         "avg_collision_steps": avg_collision_steps,
-        #         "avg_goal_steps": avg_goal_steps,
-
-        #     }
-
-        #     # Adiciona uma info de passo de treino (ou episódio) no dict
-        #     merged_metrics["training_step"] = self.n_calls
-
-        #     # Salva no arquivo .jsonl (cada linha um JSON)
-        #     with open(self.sample_file, "a") as f:
-        #         f.write(json.dumps(merged_metrics) + "\n")
 
         return True
